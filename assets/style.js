b249f7789707a296d28bfc12b88d7815cf0981eb
Daniel Gerrald <daniel.gerrald@icloud.com>
2020-08-13 15:20:42 -0400

    updating JS and index

--- assets/style.js
+++ assets/style.js
@@ -1,11 +1,15 @@
-//var search = $("userInput");
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "n&api-key=FPVcUJj8WvANRstLbU7B8rBMHW8vUA5s&limit=5";
+var search = "Abraham Lincoln" //$("#search-parameters");
+var starYear = $("#start-year");
+var endYear = $("#end-year");
+var recordsNum = $("#number-records")
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=FPVcUJj8WvANRstLbU7B8rBMHW8vUA5s&limit=5";
 
 $.ajax({
     url: queryURL,
     method: "GET"
 })
     .then(function(response){
+        console.log(response);
         //obtain response data that user selects (console.log to see how it is formatted)
         
     });
